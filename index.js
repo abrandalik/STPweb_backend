d0238dce559053770593b84c5933614585628fd7
Adam Brandalik <adam.brandalik@gmail.com>
2023-09-11 21:21:55 +0200

    first requests succesfull

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const app = require('express')();
+const res = require('express/lib/response');
+
+const express= require('express');
+const app = express();
 const PORT = 8080;
 
+app.use(express.json())
 
+app.listen(
+    PORT,
+    ()=> console.log('its alive on http://localhost:${PORT}')
+)
 
 app.get('/tshirt', (req, res) => {
     res.status(200).send({
         tshirt:'nike',
         size:'large'
     })
-}  )
+})
+
+app.post('/tshirt/:id', (req, res) => {
+
+    const {id} = req.params;
+    const {logo}= req.body;
+    res.status(200).send({
+        identification:id
+    })
+}     )
